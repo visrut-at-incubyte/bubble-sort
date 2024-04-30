24f9e0d8e42cde60e84746d27f92b628e0d87120
visrut <visrut@incubyte.co>
2024-04-30 22:11:08 +0530

    read input from user

--- sort.asm
+++ sort.asm
@@ -2,19 +2,35 @@ section .data
     inputMessage: db 'Enter Numbers: ', 0
     inputMessageLen: equ $-inputMessage
 
+    buffer: times 100 db 0
+    bufferLen: equ $-buffer
+
 section .text
     global _start
+
+    %define STDIN 0
+    %define STDOUT 1
+    %define SYS_READ 0
+    %define SYS_WRITE 1
+    %define SYS_EXIT 60
 
 _start:
     
     ; Print input message
-    mov rax, 1
-    mov rdi, 1
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
     mov rsi, inputMessage
     mov rdx, inputMessageLen
     syscall
 
+    ; Read input
+    mov rax, SYS_READ
+    mov rdi, STDIN
+    mov rsi, buffer
+    mov rdx, bufferLen
+    syscall
+
     ; Exit
-    mov rax, 60
+    mov rax, SYS_EXIT
     xor rdi, rdi
     syscall
